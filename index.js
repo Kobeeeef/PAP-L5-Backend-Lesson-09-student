93c8c648184c302b11d07b6a276ad178b03caa59
kobel <kobelei335@gmail.com>
2024-11-14 16:55:19 -0800

    lesson 11

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
 const cors = require('cors');
 const express = require('express');
 const { connectToDB } = require("./connect");
+const { getAllMessages } = require('./models/read-messages');
+const { createNewMessage } = require('./models/create-message');
+const { updateMessage } = require('./models/update-messages');
+const { deleteMessage } = require('./models/delete-message');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-
 app.use(cors());
+app.use(express.json());  // Middleware to handle JSON requests
 
-//Home route
+
 app.get('/', (req, res) => {
     res.send("Welcome to my API home page!")
-    console.log("GET request received on home page")
 });
 
-//Subroute
-app.get('/message', (req, res) => {
-    res.send("Surprise! I am a ninja!")
-    console.log("GET request received on message route")
+app.get('/:secret', async (req, res) => {
+    try {
+        const results = await getAllMessages(req.params.secret);
+        res.send(results);
+        console.log("GET request received on home page with secret");
+    } catch (e) {
+        console.error("Error fetching messages:", e);
+        res.sendStatus(500);
+    }
 });
 
-//Dynamic route
-app.post('/message/:secret', (req, res) => {
-    res.json({ secret: "Message received: " + req.params.secret });
-    console.log("Secret message received by POST request")
+
+app.post('/message', async (req, res) => {
+    try {
+        const newMessage = req.body;
+        await createNewMessage(newMessage);
+        res.sendStatus(201);
+        console.log("POST request received to create a new message");
+    } catch (e) {
+        console.error("Error creating message:", e);
+        res.sendStatus(500);
+    }
 });
 
 
+app.patch('/message/:id', async (req, res) => {
+    try {
+        const editedMessage = req.body;
+        await updateMessage(req.params.id, editedMessage);
+        res.sendStatus(200);
+        console.log("PATCH request received to update a message");
+    } catch (e) {
+        console.error("Error updating message:", e);
+        res.sendStatus(500);
+    }
+});
 
-//* ********************* Launching the server **************** */
+app.delete('/message/:id', async (req, res) => {
+    try {
+        const messageId = req.params.id;
+        await deleteMessage(messageId);
+        res.sendStatus(200);
+        console.log("DELETE request received to delete a message");
+    } catch (e) {
+        console.error("Error deleting message:", e);
+        res.sendStatus(500);
+    }
+});
+
+
 start()
 app.listen(port, () => console.log(`Server running on port ${port}...`));
 
@@ -36,6 +74,6 @@ async function start() {
     try {
         await connectToDB();
     } catch (e) {
-        console.error(e);
+        console.error("Error connecting to database:", e);
     }
 }
